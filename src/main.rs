960cbcd4a641410384f378b3dad0865b507a4887
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2022-12-10 17:16:24 +0200

    Add CLI and REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use clap::Parser;
+use rustyline::error::ReadlineError;
+use rustyline;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
+struct Args {
+    #[arg(long)]
+    debug: bool
+}
+
+const PROMPT: &'static str = "fvm> ";
+const HISTORY_FILENAME: &'static str = ".fvm-history.txt";
+    
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", Args::parse());
+
+    let mut rl = rustyline::Editor::<()>::new().unwrap();
+
+    if rl.load_history(HISTORY_FILENAME).is_err() {
+        println!("No previous history.");
+    }
+
+    loop {
+        match rl.readline(PROMPT) {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+
+                println!("{}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    rl.save_history(HISTORY_FILENAME).unwrap();
 }
